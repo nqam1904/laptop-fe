fd03aeaa5b00292f2bc6ae54f016e448145a8352
nqam <nghiemminh1904@gmail.com>
2022-07-26 22:03:44 +0700

    fgf

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -7,6 +7,7 @@ const axiosClient = axios.create({
 	headers: {
 		'Content-Type': 'application/json',
 		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Headers": "*",
 		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 		'Access-Control-Allow-Credentials': true
 	},

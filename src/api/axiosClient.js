5d8a2d5acd8a7e03f3eef49cd158a062f450acf4
nqam <nghiemminh1904@gmail.com>
2022-07-26 22:11:58 +0700

    d

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -7,7 +7,7 @@ const axiosClient = axios.create({
 	headers: {
 		'Content-Type': 'application/json',
 		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
+		"Access-Control-Allow-Headers": "Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Access-Control-Allow-Origin,access-control-allow-origin",
 		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 		'Access-Control-Allow-Credentials': true
 	},

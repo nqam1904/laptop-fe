a9732fbc969fac762042a6135579e1a5de416461
nqam <nghiemminh1904@gmail.com>
2022-08-06 14:07:06 +0700

    fix layout

--- src/constants/common.js
+++ src/constants/common.js
@@ -2,7 +2,7 @@ import configApi from "api/configApi";
 let title_website = ''
 export const getConfigWeb = async () => {
    let getConfig = await configApi.getConfig();
-   document.title = getConfig?.[0]?.title_website || 'Lap4all'
+   document.title = getConfig?.[0]?.content || 'Lap4all - Thịnh'
    title_website = getConfig?.[0]?.title_website || 'Lap4all'
 }
 export { title_website }

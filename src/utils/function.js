a963f4f2f0da9ce64a7c3faa086b8b3b42f4318f
nqam <nghiemminh1904@gmail.com>
2022-08-13 13:03:06 +0700

    cm

--- src/utils/function.js
+++ src/utils/function.js
@@ -1,3 +1,7 @@
-export const formatNumber = (number) => number?.toString()?.replace(/\d(?=(\d{3})+$)/g, '$&,');
+export const formatNumber = (number) => {
+
+   let price = number * 1000
+   return price?.toString()?.replace(/\d(?=(\d{3})+$)/g, '$&,');
+};
 export const formatChar = (string) => string?.split('_')?.join(' ') || ''
 export const formatSizeDisplay = string => string?.split('_').filter(x => x)?.join('.') + '\"' || '';

84acfdf5569af2e979911e72481c6a88b09511f9
Anh Minh Nghiêm Quốc <nghiemminh1904@gmail.com>
2023-04-06 23:41:47 +0700

    add chart

--- src/pages/detail/ProductDetail.js
+++ src/pages/detail/ProductDetail.js
@@ -1,11 +1,21 @@
 import swal from '@sweetalert/with-react'
 import ProductApi from 'api/productApi'
 import { images } from 'assets'
+import {
+	Chart as ChartJS,
+	Filler,
+	Legend,
+	LineElement,
+	PointElement,
+	RadialLinearScale,
+	Tooltip,
+} from 'chart.js'
 import { Breadcrumb, SliderSyncing, TableTechnique } from 'components'
 import Layout from 'layouts/Layout'
 import _ from 'lodash'
 import Markdown from 'markdown-to-jsx'
 import { useEffect } from 'react'
+import { Radar } from 'react-chartjs-2'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
@@ -14,13 +24,21 @@ import { footerSelector } from 'redux/selector/footerSelector'
 import { detailLaptopSelector } from 'redux/selector/laptopSelector'
 import { formatChar, formatNumber, formatSizeDisplay } from 'utils/function'
 import './ProducDetail.scss'
-
 const ProductDetail = () => {
 	const detailLaptop = useSelector(detailLaptopSelector)
+	const {
+		coding = 0,
+		design = 0,
+		video = 0,
+		office = 0,
+		gaming = 0,
+		graphic = 0,
+	} = detailLaptop || {}
 	const footer = useSelector(footerSelector)
 	const location = useLocation()
 	const laptopSlug = location.pathname.replace('/product/', '')
 	const dispatch = useDispatch()
+	ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend)
 	const display =
 		formatSizeDisplay(detailLaptop?.size_display) +
 			'' +
@@ -40,7 +58,35 @@ const ProductDetail = () => {
 		// dispatch(getProdcutViewAction(detailLaptop))
 		updateView()
 	}, [])
-
+	const options = {
+		responsive: true,
+		maintainAspectRatio: true, // Set to false to set width and height manually
+		width: 100, // Manually set width
+		height: 100, // Manually set height,
+		scale: {
+			ticks: {
+				beginAtZero: true,
+				fontWeight: 'bold', // Set the font weight to bold
+			},
+		},
+	}
+	const dataChart = {
+		labels: ['Học tập, văn phòng', 'Lập trình', 'Gaming', 'Designing', '3D Design', 'Video'],
+		datasets: [
+			{
+				label: 'Cấu hình và đặc điểm',
+				data: [office, coding, gaming, design, graphic, video],
+				fill: true,
+				backgroundColor: 'rgba(255, 99, 132, 0.2)',
+				borderColor: 'rgb(255, 99, 132)',
+				pointBackgroundColor: 'rgb(255, 99, 132)',
+				pointBorderColor: '#fff',
+				pointHoverBackgroundColor: '#fff',
+				pointHoverBorderColor: 'rgb(255, 99, 132)',
+				borderWidth: 1,
+			},
+		],
+	}
 	const updateView = () => {
 		let view = detailLaptop?.view
 		ProductApi.updateViewLaptop({ id: detailLaptop?.id, view: ++view })
@@ -174,6 +220,9 @@ const ProductDetail = () => {
 							<Markdown className="content_view">{detailLaptop?.description || ''}</Markdown>
 						</TabPanel>
 						<TabPanel style={{ paddingLeft: 20, marginTop: 20 }}>
+							<div className="chart">
+								<Radar data={dataChart} options={options} />
+							</div>
 							<TableTechnique configuration={detailLaptop} />
 						</TabPanel>
 					</Tabs>

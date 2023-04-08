62f605634db64a0be80f779107adb2f5d6639a1d
Anh Minh Nghiêm Quốc <nghiemminh1904@gmail.com>
2023-04-08 16:29:20 +0700

    add key facebook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { getConfigWeb } from 'constants/common'
-import { AccessoryByCate, AccessoryDetail, Home, LaptopByCate, NotFound, ProductDetail, SearchProduct } from 'pages'
+import {
+	AccessoryByCate,
+	AccessoryDetail,
+	Home,
+	LaptopByCate,
+	NotFound,
+	ProductDetail,
+	SearchProduct,
+} from 'pages'
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -11,13 +19,18 @@ import { getFooterAction } from 'redux/actions/footerAction'
 import { getHeaderAction } from 'redux/actions/headerAction'
 import { getCategoryAction, getListLaptopAction } from 'redux/actions/laptopAction'
 import { headerSelector } from 'redux/selector/headerSeletor'
+import { appId, pageId } from 'utils/constant'
 const App = ({ history }) => {
 	const header = useSelector(headerSelector)
 	const dispatch = useDispatch()
 
 	const routeLaptop = () => {
 		const listHeader = header?.map((item, index) => (
-			<Route key={index} path={item?.url} element={<LaptopByCate title={item.categories[0]?.name} />} />
+			<Route
+				key={index}
+				path={item?.url}
+				element={<LaptopByCate title={item.categories[0]?.name} />}
+			/>
 		))
 		return listHeader || []
 	}
@@ -36,6 +49,25 @@ const App = ({ history }) => {
 			storage.removeItem('persist:root')
 		}
 	}, [])
+	useEffect(() => {
+		window.fbAsyncInit = function () {
+			window.FB.init({
+				appId: appId,
+				cookie: true,
+				xfbml: true,
+				version: 'v12.0',
+			})
+		}
+		;(function (d, s, id) {
+			var js,
+				fjs = d.getElementsByTagName(s)[0]
+			if (d.getElementById(id)) return
+			js = d.createElement(s)
+			js.id = id
+			js.src = 'https://connect.facebook.net/en_US/sdk.js'
+			fjs.parentNode.insertBefore(js, fjs)
+		})(document, 'script', 'facebook-jssdk')
+	}, [])
 	return (
 		<Routes history={history}>
 			<Route path="/" element={<Home />} />
@@ -45,6 +77,7 @@ const App = ({ history }) => {
 			<Route path="/search" element={<SearchProduct />} />
 			<Route path="/accessory" element={<AccessoryByCate title="Accessory" />} />
 			<Route path="*" element={<NotFound />} />
+			<div className="fb-customerchat" attribution="setup_tool" page_id={pageId}></div>
 		</Routes>
 	)
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,12 +1,14 @@
-const API_URL = process.env.REACT_APP_URL;
-const dev = process.env.ENVIROMENT;
+const API_URL = process.env.REACT_APP_URL
+const dev = process.env.ENVIROMENT
+const pageId = process.env.PAGE_ID_FACEBOOK
+const appId = process.env.APP_ID_FACEBOOK
 const screen = {
-   mobile: 768,
-   minTablet: 768,
-   maxTablet: 1024,
-   desktop: 1024,
-};
+	mobile: 768,
+	minTablet: 768,
+	maxTablet: 1024,
+	desktop: 1024,
+}
 const screenWidth = window.screen.width
 const screenHeight = window.screen.height
 
-export { API_URL, screen, dev, screenWidth, screenHeight }
+export { API_URL, screen, dev, screenWidth, screenHeight, pageId, appId }
